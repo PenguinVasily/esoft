f3205c84a7a6f9244fe3cda177c4c7ad3f33f4db
Semyon Nikonov <PFRVasily@gmail.com>
2023-09-11 21:07:52 +0300

    Удаление неиспользуемого метода

--- backend/src/database/postgresql.ts
+++ backend/src/database/postgresql.ts
@@ -7,20 +7,6 @@ import process from "process";
 export class PostgreSQL {
     client: Knex;
 
-    private static getConnectionString() {
-        if (process.env.POSTGRESQL_DSN) {
-            return process.env.postgresql_dsn
-        }
-
-        const host = process.env.POSTGRESQL_HOST
-        const port = process.env.POSTGRESQL_PORT
-        const user = process.env.POSTGRESQL_USER
-        const password = process.env.POSTGRESQL_PASSWORD
-        const database = process.env.POSTGRESQL_DATABASE
-
-        return `postgresql://${user}:${password}@${host}:${port}/${database}`
-    }
-
     constructor() {
         this.client = knex({
             client: 'postgresql',
